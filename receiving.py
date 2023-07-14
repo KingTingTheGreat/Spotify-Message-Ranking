a96ac60b457d963dcf4b0218098fc6020c1233c0
KingTingTheGreat <jting@bu.edu>
2023-07-14 09:08:38 -0400

    following documentation more

--- receiving.py
+++ receiving.py
@@ -9,26 +9,33 @@ load_dotenv()
 app = Flask(__name__)
 
 @app.route('/sms', methods=['GET', 'POST'])
-def sms_print():
+def incoming_sms():
     body = request.values.get('Body', None)
+    resp = MessagingResponse()
     if body is None:
         return ''
+    resp.message(f'You said: {body}')
+    return str(resp)
+# def sms_print():
+#     body = request.values.get('Body', None)
+#     if body is None:
+#         return ''
     
-    response = MessagingResponse()
-    response.message(f'You said: {body}')
-    # response.message('Hello from Twilio!')
-    # print(str(response))
-    print(response)
-    print(str(response))
+#     response = MessagingResponse()
+#     response.message(f'You said: {body}')
+#     # response.message('Hello from Twilio!')
+#     # print(str(response))
+#     print(response)
+#     print(str(response))
     
-    ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
-    AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
-    TWILIO_NUMBER:str = os.getenv('TWILIO_NUMBER')
-    DEST_NUMBER:str = os.getenv('DEST_NUMBER')
-    assert ACCOUNT_SID is not None and AUTH_TOKEN is not None
+#     ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
+#     AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
+#     TWILIO_NUMBER:str = os.getenv('TWILIO_NUMBER')
+#     DEST_NUMBER:str = os.getenv('DEST_NUMBER')
+#     assert ACCOUNT_SID is not None and AUTH_TOKEN is not None
 
-    client:Client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    message = client.messages.create(body=f'Message: {body}', from_=TWILIO_NUMBER, to=DEST_NUMBER)
+#     client:Client = Client(ACCOUNT_SID, AUTH_TOKEN)
+#     message = client.messages.create(body=f'Message: {body}', from_=TWILIO_NUMBER, to=DEST_NUMBER)
 
 if __name__ == '__main__':
     print('Starting Flask app')
