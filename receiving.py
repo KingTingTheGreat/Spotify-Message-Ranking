20ce506d1b3318015a3efe98715e62927bbad7b0
KingTingTheGreat <jting@bu.edu>
2023-07-18 13:09:53 -0400

    variables saved as constants. test messages

--- receiving.py
+++ receiving.py
@@ -5,6 +5,10 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
+ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
+AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_NUMBER:str = os.getenv('TWILIO_NUMBER')
+DEST_NUMBER:str = os.getenv('DEST_NUMBER')
 
 app = Flask(__name__)
 
@@ -16,10 +20,7 @@ def incoming_sms():
         return ''
     resp.message(f'You said: {body}')
     
-    ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
-    AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
-    TWILIO_NUMBER:str = os.getenv('TWILIO_NUMBER')
-    DEST_NUMBER:str = os.getenv('DEST_NUMBER')
+
     assert ACCOUNT_SID is not None and AUTH_TOKEN is not None
 
     client:Client = Client(ACCOUNT_SID, AUTH_TOKEN)
@@ -27,4 +28,6 @@ def incoming_sms():
 
 if __name__ == '__main__':
     print('Starting Flask app')
+    c = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = c.messages.create(body='starting Flask application!', from_=TWILIO_NUMBER, to=DEST_NUMBER)
     app.run(debug=True)
