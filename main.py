4af4d5ce8e21e946f5eeee0584a5b63bb92a3b5b
KingTingTheGreat <jting@bu.edu>
2023-07-20 15:06:13 -0400

    renamed variables

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import time
 from datetime import datetime
-from stats import get_message
+from stats import get_messages
 from datetime import datetime
 from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
-ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
-AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_NUMBER:str = os.getenv('TWILIO_NUMBER')
 
 def is_last_day_of_month() -> bool:
@@ -24,21 +24,29 @@ def is_last_day_of_month() -> bool:
         return day == 30
     else:
         return day == 31
+    
+
+def send_message(message:str) -> None:
+    """ sends a message to the user """
+    c = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = c.messages.create(body='a message was received', from_=TWILIO_NUMBER, to='')
 
 
 def main():
-    last_month_sent = datetime.now().month - 1
+    last_month_sent = datetime.now().month - 1  # ensures we send a message at the end of the current month
     while True:
         if not is_last_day_of_month() or last_month_sent == datetime.now().month:
             time.sleep(60)
             continue
         last_month_sent = datetime.now().month
-        message = get_message()
-        if message == '':
+        messages = get_messages()
+        if not messages:
             time.sleep(60)
             continue
-        c = Client(ACCOUNT_SID, AUTH_TOKEN)
-        message = c.messages.create(body=message, from_=TWILIO_NUMBER, to='')
+        for message in messages:
+            if not message:
+                continue
+            send_message(message)
         time.sleep(60)
 
 
