4e7920b4969329347c2b2152b471cfb85d93cb92
KingTingTheGreat <jting@bu.edu>
2023-09-05 14:53:52 -0400

    split get_songs_username() into two functions. improved error handling

--- stats.py
+++ stats.py
@@ -14,7 +14,7 @@ CALLBACK_URL = 'https://example.com/callback/'
 MONTHS = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June',
         7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
 
-def get_songs_username() -> tuple[str, str]:
+def get_top_songs() -> tuple[str, str]:
     # Spotify client
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -25,18 +25,27 @@ def get_songs_username() -> tuple[str, str]:
 
     # get top 10 songs from last 30 days
     top_songs = sp.current_user_top_tracks(time_range="short_term", limit=10)
-    top_songs = [(song['name'], song['artists'][0]['name']) for song in top_songs['items']]
+    top_songs = [(song.get('name', '[Song Name]'), song.get('artists', {'name':'[Artist]'})[0]['name']) for song in top_songs.get('items', [])]
 
+    return top_songs
+
+
+def get_username() -> str:
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=CALLBACK_URL,
+        scope="user-top-read"
+    ))
     # get user's display name
     user_info = sp.current_user()
-    display_name = user_info['display_name']
+    return user_info.get('display_name', '[Your Name]')
 
-    return (top_songs, display_name)
 
 def get_messages() -> str:
     """ returns a string of the message to be sent to the user """
     month, year = datetime.now().month, datetime.now().year
-    top_songs, name = get_songs_username()
+    top_songs, name = get_top_songs(), get_username()
     if top_songs is None or name is None:
         return ''
     top_songs = '\n'.join([f'{i+1}. {song[0]} - {song[1]}' for i, song in enumerate(top_songs[:10])])
