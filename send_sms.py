12e139e2ce8149d87bf80f26d83363c1f94af45b
KingTingTheGreat <jting@bu.edu>
2023-10-12 18:42:25 -0400

    uses constants

--- send_sms.py
+++ send_sms.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 from twilio.rest import Client
 from dotenv import load_dotenv
 
@@ -6,14 +6,14 @@ load_dotenv()
 
 ACCOUNT_SID:str = os.getenv('TWILIO_ACCOUNT_SID')
 AUTH_TOKEN:str = os.getenv('TWILIO_AUTH_TOKEN')
-NUMBER:int = int(os.getenv('TWILIO_NUMBER'))
+TWILIO_NUMBER:str = int(os.getenv('TWILIO_NUMBER'))
+DEST_NUMBER:str = int(os.getenv('DEST_NUMBER'))
+
 assert ACCOUNT_SID is not None and AUTH_TOKEN is not None
 
 client:Client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 message_body = input('Enter your message: ')
-# destination_number = input('+19177575886')
-destination_number = '+19177575886'
 
-message = client.messages.create(body=message_body, from_=NUMBER, to=destination_number)
-print(message.sid)
+message = client.messages.create(body=message_body, from_=TWILIO_NUMBER, to=DEST_NUMBER)
+print(message.status)
