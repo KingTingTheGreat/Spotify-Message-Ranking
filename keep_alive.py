ed9016e671c00571295c01dd2693b80d5528444d
KingTingTheGreat <jting@bu.edu>
2023-10-20 15:13:27 -0400

    exported functions to separate file ;

--- keep_alive.py
+++ keep_alive.py
@@ -1,46 +1,11 @@
-import os
-import time
 import datetime
 import oracledb
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TABLE_NAME = 'user_data'
-
-ORACLE_USERNAME = os.getenv('ORACLE_DATABASE_ADMIN_USERNAME')
-ORACLE_PASSWORD = os.getenv('ORACLE_DATABASE_ADMIN_PASSWORD')
-ORACLE_DSN = os.getenv('ORACLE_DATABASE_DSN')
-ORACLE_WALLET_LOCATION = os.getenv('ORACLE_DATABASE_WALLET_LOCATION')
-ORACLE_WALLET_PASSWORD = os.getenv('ORACLE_DATABASE_WALLET_PASSWORD')
-
-def count_users(connection:oracledb.Connection) -> int:
-    with connection.cursor() as cursor:
-        cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}")
-        count = cursor.fetchone()[0]
-        return count
-
-def connect_to_database():
-    try:
-        print('Connecting to Oracle Database...')
-        connection = oracledb.connect(
-            user=ORACLE_USERNAME,
-            password=ORACLE_PASSWORD,
-            dsn=ORACLE_DSN,
-            wallet_location=ORACLE_WALLET_LOCATION,
-            wallet_password=ORACLE_WALLET_PASSWORD
-        )
-    except oracledb.DatabaseError as e:
-        print('Failed to connect to Oracle Database')
-        print("The database returned the following error:", e)
-        return  
-    print('Successfully connected to Oracle Database')
-    return connection
+from database_functions import num_rows, PRODUCTION_TABLE
 
 if __name__ == '__main__':
-    connection = connect_to_database()
+    num_users = num_rows(PRODUCTION_TABLE)
 
-    data = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Number of users: {count_users(connection)}"
+    data = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Number of users: {num_users}"
     print(data)
 
     with open('log.txt', 'a') as f:
