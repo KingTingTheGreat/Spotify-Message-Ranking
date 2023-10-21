eafafc03556613d4acc2de879a2e4da69467172e
KingTingTheGreat <jting@bu.edu>
2023-10-21 12:13:45 -0400

    working outline of taking in phone numbers, get auth codes, and adding this data to database

--- app.py
+++ app.py
@@ -1,78 +1,71 @@
 import os
+import random
+import string
 from dotenv import load_dotenv
-from flask import Flask, request, redirect, session, url_for
-import spotipy
-import spotipy.util as util
-from spotipy.oauth2 import SpotifyOAuth
-import oracledb
-from keep_alive import connect_to_database, count_users
+from flask import Flask, request, redirect, url_for
 from flask_cors import CORS
+from database_functions import PRODUCTION_TABLE, num_rows, contains, add_to_table
+from spotify_functions import generate_authorization_url
 
 load_dotenv()
 
+# stores a state key and phone number value
+state_numbers = {}
+
+FLASK_SECRET_KEY = os.environ['FLASK_SECRET_KEY']
 app = Flask(__name__)
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+
 cors = CORS(app, resources={r"/*": {"origins": ["http://localhost/*", "http://127.0.0.1/*"]}})
 # cors = CORS(app, resources={r"/*": {"origins": '*'}})
 
-TABLE_NAME = 'user_data'
 
-# Spotify API connection info
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-# SPOTIFY_REDIRECT_URI = os.environ.get('SPOTIFY_REDIRECT_URI')
-SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback/'
-sp_auth = SpotifyOAuth(
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET,
-    redirect_uri=SPOTIFY_REDIRECT_URI,
-    scope="user-top-read"
-)
-
-
-# Flask info
-FLASK_SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+def gen_state(n=16):
+    """ returns a random string of length n that we use as a state key """
+    return ''.join(random.choice(string.ascii_letters) for _ in range(n))
 
 
 @app.route('/')
 def index():
-    auth_url = sp_auth.get_authorize_url()
-    return redirect(auth_url)
-    # return "Welcome to the website! Please enter your phone number."
+    # auth_url = sp_auth.get_authorize_url()
+    # return redirect(auth_url)
+    return "Welcome to the website! Please enter your phone number."
 
-@app.route('/api/callback', methods=['GET', 'POST'])
+
+@app.route('/callback', methods=['GET'])
 def callback():
-    token_info = sp_auth.get_access_token(request.args.get('code'))
-    session['token_info'] = token_info
-    print(token_info)
-    # return redirect(url_for('index'))
+    auth_code = request.args.get('code', None)
+    # session['token_info'] = token_info
+    print(f"AUTH CODE: {auth_code}")
+    phone_number = state_numbers.pop(request.args.get('state', None), None)
+    print(f"PHONE NUMBER: {phone_number}")
+    assert auth_code is not None and phone_number is not None
 
-@app.route('/api/contains', methods=['GET'])
-def contains():
-    """ returns boolean indicating if input phone number is in database"""
+    add_to_table(PRODUCTION_TABLE, phone_number, auth_code)
+
+    # redirect to front end homepage; will be changed in the future
+    return redirect("https://kingtingthegreat.github.io/Spotify-Message-Ranking/")
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    # get input phone number
     phone_number = request.args.get('phone_number')
-    print(f"Checking for: {phone_number}")
-    # check if phone number is in database
-    with connect_to_database() as connection:
-        assert(connection is not None)
-        with connection.cursor() as cursor:
-            cursor.execute(f"SELECT * FROM {TABLE_NAME} WHERE PHONENUMBER = '{phone_number}'")
-            result = cursor.fetchone()
-            if result is None:
-                return "false"
-            else:
-                return "true"
+
+    # already signed up, tell user this
+    if contains(phone_number):
+        return redirect("https://kingtingthegreat.github.io/Spotify-Message-Ranking/already_signed_up.html")
     
-def add_to_database(phone_number: str, auth_code: str = 'test-auth') -> bool:
-    """ adds phone number - auth code pair to our database """
-    with connect_to_database() as connection:
-        assert(connection is not None)
-        with connection.cursor() as cursor:
-            cursor.execute(f"INSERT INTO {TABLE_NAME} (PHONENUMBER, AUTHCODE) VALUES ('{phone_number}', '{auth_code}')")
-        connection.commit()
-            
+    # store phone number in hashtable temporarily
+    state = gen_state()
+    state_numbers[state] = phone_number
+
+    # redirect to spotyify auth page
+    redirect(generate_authorization_url(state))
+
 
 if __name__ == '__main__':
     # add_to_database('1234567890')
-    print(f"Num Users: {count_users(connect_to_database())}")
+    print(f"Num Users: {num_rows(PRODUCTION_TABLE)}")
     app.secret_key = FLASK_SECRET_KEY
     app.run(debug=True, port=8888)
