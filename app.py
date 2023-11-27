da77d4d7c68c77e231450354bca82a1ba1ec2b81
KingTingTheGreat <jting@bu.edu>
2023-11-26 22:24:11 -0500

    styling. updated error message because this service is no longer operational

--- app.py
+++ app.py
@@ -12,75 +12,86 @@ load_dotenv()
 # stores a state key and phone number value
 state_numbers = {}
 
-FLASK_SECRET_KEY = os.environ['FLASK_SECRET_KEY']
+FLASK_SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
 app = Flask(__name__)
-app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+app.config["SECRET_KEY"] = FLASK_SECRET_KEY
 
-cors = CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1*", "https://accounts.spotify.com/authorize*"]}})
+cors = CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": ["http://127.0.0.1*", "https://accounts.spotify.com/authorize*"]
+        }
+    },
+)
 # cors = CORS(app, resources={r"/*": {"origins": '*'}})
 
 
 def gen_state(n=16):
-    """ returns a random string of length n that we use as a state key """
-    return ''.join(random.choice(string.ascii_letters) for _ in range(n))
+    """returns a random string of length n that we use as a state key"""
+    return "".join(random.choice(string.ascii_letters) for _ in range(n))
 
 
-@app.route('/')
+@app.route("/")
 def index():
     # auth_url = sp_auth.get_authorize_url()
     return redirect("https://kingtingthegreat.github.io/Spotify-Message-Ranking/")
     return "Welcome to the website! Please enter your phone number."
 
 
-@app.route('/callback', methods=['GET'])
+@app.route("/callback", methods=["GET"])
 def callback():
     print(state_numbers)
-    auth_code = request.args.get('code', None)
+    auth_code = request.args.get("code", None)
     # session['token_info'] = token_info
     print(f"AUTH CODE: {auth_code}")
-    state = request.args.get('state', None)
+    state = request.args.get("state", None)
     print(f"STATE: {state}")
-    phone_number = state_numbers.pop(request.args.get('state', None), None)
+    phone_number = state_numbers.pop(request.args.get("state", None), None)
     print(f"PHONE NUMBER: {phone_number}")
 
     if auth_code is None or phone_number is None:
-        return Response('Error: invalid auth code or phone number', status=400, mimetype='application/json')
+        return Response(
+            "Error: invalid auth code or phone number",
+            status=400,
+            mimetype="application/json",
+        )
 
     add_to_table(PRODUCTION_TABLE, phone_number, auth_code)
 
     # redirect to front end homepage; will be changed in the future
-    return redirect('http://127.0.0.1:5500/index.html')
+    return redirect("http://127.0.0.1:5500/index.html")
     return redirect("https://kingtingthegreat.github.io/Spotify-Message-Ranking/")
 
 
-@app.route('/api/signup', methods=['POST'])
+@app.route("/api/signup", methods=["POST"])
 def signup():
-    print(f'REQUEST ARGS: {request.args}')
+    print(f"REQUEST ARGS: {request.args}")
     # get input phone number
-    phone_number = request.args.get('phone_number', None)
+    phone_number = request.args.get("phone_number", None)
     # data = request.get_data().decode('utf-8')
     # print(f'DATA: {data}')
-    print(f'PHONE NUMBER: {phone_number}')
+    print(f"PHONE NUMBER: {phone_number}")
 
     # already signed up, tell user this
-    if contains(PRODUCTION_TABLE, phone_number):
+    if phone_number is None or contains(PRODUCTION_TABLE, phone_number):
         print("Already signed up")
-        return Response('Already signed up!', status=400, mimetype='application/json')
-    
+        return Response("Already signed up!", status=400, mimetype="application/json")
+
     # store phone number in hashtable temporarily
-    print('New user, adding to database')
+    print("New user, adding to database")
     state = gen_state()
     state_numbers[state] = phone_number
 
     # redirect to spotyify auth page
     redir = generate_authorization_url(state)
     print(f"REDIR: {redir}")
-    return jsonify({'redirect': redir})
+    return jsonify({"redirect": redir})
     return redirect(redir)
     # return redirect(generate_authorization_url(state))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # add_to_database('1234567890')
     print(f"Num Users: {num_rows(PRODUCTION_TABLE)}")
     app.secret_key = FLASK_SECRET_KEY
--- src/js/main.js
+++ src/js/main.js
@@ -1,130 +1,130 @@
-
 /**
  * @returns a phone number with all non-numeric characters removed
  * additionally, removes the leading 1 if the phone number is 11 digits long
  */
 function cleanPhoneNumber(phone_number) {
-    // Remove all non-numeric characters
-    phone_number = phone_number.replace(/\D/g, '');
-    // assume the user is in the US
-    if (phone_number.length === 11 && phone_number[0] === '1') {
-        phone_number = phone_number.slice(1);
-    }
-    // ensure the phone number is 10 digits long
-    else if (phone_number.length !== 10) {
-        return null;
-    }
-    return phone_number;
+	// Remove all non-numeric characters
+	phone_number = phone_number.replace(/\D/g, "");
+	// assume the user is in the US
+	if (phone_number.length === 11 && phone_number[0] === "1") {
+		phone_number = phone_number.slice(1);
+	}
+	// ensure the phone number is 10 digits long
+	else if (phone_number.length !== 10) {
+		return null;
+	}
+	return phone_number;
 }
 
 /**
  * makes a request to the server to check if the phone number is already in the database
-*/
+ */
 function fetchContainsNumber(number, callback) {
-    // api url
-    const URL = `http://127.0.0.1:8888/api/contains?phone_number=${number}`;
-    // const URL = `https://spotify-message-ranking.vercel.app/api/contains?phone_number=${number}`;
-    // make a request to the url
-    fetch(URL)
-        .then(response => response.text())
-        .then(data => {
-            callback(data, null);
-        })
-        .catch(error => {
-            callback(null, error);
-        });
+	// api url
+	const URL = `http://127.0.0.1:8888/api/contains?phone_number=${number}`;
+	// const URL = `https://spotify-message-ranking.vercel.app/api/contains?phone_number=${number}`;
+	// make a request to the url
+	fetch(URL)
+		.then((response) => response.text())
+		.then((data) => {
+			callback(data, null);
+		})
+		.catch((error) => {
+			callback(null, error);
+		});
 }
 
 /**
- * 
+ *
  * @returns true if the phone number is already in the database, false otherwise
  */
 function submitPhoneNumberOld() {
-    const input_number = document.getElementById('phone_number').value;
-    const phone_number = cleanPhoneNumber(input_number);
-    console.log(`PhoneNumber to check is ${phone_number}`);
+	const input_number = document.getElementById("phone_number").value;
+	const phone_number = cleanPhoneNumber(input_number);
+	console.log(`PhoneNumber to check is ${phone_number}`);
 
-    if (phone_number === null) {
-        console.log("Please enter a valid phone number.");
-        // alert("Please enter a valid phone number.");
-        return;
-    }
-    fetchContainsNumber(phone_number, (data, error) => {
-        if (error) {
-            console.error("Error:", error);
-            // alert("An error occurred. Please try again later.");
-        }
-        if (data === "true") {
-            // alert("This phone number has already been signed up for this service.");
-            console.log(data);
-            console.log("This phone number has already been signed up for this service.");
-        } else if (data == "false") {
-            // alert("You have been signed up for this messaging service!");
-            console.log(data);
-            console.log("Success! You have been signed up for this service.");
-        }
-        else {
-            console.log(data);
-            console.log("Error: data is not a boolean value.");
-        }
-    });
+	if (phone_number === null) {
+		console.log("Please enter a valid phone number.");
+		// alert("Please enter a valid phone number.");
+		return;
+	}
+	fetchContainsNumber(phone_number, (data, error) => {
+		if (error) {
+			console.error("Error:", error);
+			// alert("An error occurred. Please try again later.");
+		}
+		if (data === "true") {
+			// alert("This phone number has already been signed up for this service.");
+			console.log(data);
+			console.log("This phone number has already been signed up for this service.");
+		} else if (data == "false") {
+			// alert("You have been signed up for this messaging service!");
+			console.log(data);
+			console.log("Success! You have been signed up for this service.");
+		} else {
+			console.log(data);
+			console.log("Error: data is not a boolean value.");
+		}
+	});
 }
 
 function submitPhoneNumber() {
-    const inputNumber = document.getElementById('phone_number').value;
-    const phoneNumber = cleanPhoneNumber(inputNumber);
-    console.log(phoneNumber);
+	const inputNumber = document.getElementById("phone_number").value;
+	const phoneNumber = cleanPhoneNumber(inputNumber);
+	console.log(phoneNumber);
 
-    if (phoneNumber === null) {
-        console.log("Please enter a valid phone number.");
-        document.getElementById('error-message').innerHTML = "Please enter a valid phone number.";
-        return;
-    }
+	if (phoneNumber === null) {
+		console.log("Please enter a valid phone number.");
+		document.getElementById("error-message").innerHTML = "Please enter a valid phone number.";
+		return;
+	}
 
-    // const url = 'https://spotify-message-ranking.vercel.app/' + phoneNumber;
-    // const url = 'https://spotify-message-ranking.vercel.app/api/signup';
-    const url = 'http://127.0.0.1:8888/api/signup'
+	// due to updated A2P 10DLC regulations, this service is no longer available
+	document.getElementById("error-message").innerHTML =
+		"Due to updated A2P 10DLC regulations, this service is no longer available.";
+	return;
 
-    const fetchUrl = `${url}?phone_number=${phoneNumber}`;
-    fetch(fetchUrl, {
-        method: 'POST',
-        // redirect: 'follow',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.redirect) {
-                window.location.replace(data.redirect);
-            }
-            else {
-                document.getElementById('error-message').innerHTML = 'something went wrong';
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-    // .then(response => {
-    //     console.log(`response: ${response}`)
-    //     if (response.status === 200) {
-    //         document.getElementById('error-message').innerHTML = "You have been signed up for this messaging service!";
-    //         console.log(response.text());
-    //         // console.log("Success! You have been signed up for this service.");
-    //     }
-    //     else if (response.status === 400) {
-    //         document.getElementById('error-message').innerHTML = "This phone number has already been signed up.";
-    //         console.log(`error response - 400: ${response}`);
-    //         // window.location.href = "http://127.0.0.1:5500/retry.html"
-    //     }
-    //     else {
-    //         document.getElementById('error-message').innerHTML = "An error occurred. Please try again later.";
-    //         console.log(`error response - other: ${response}`);
-    //     }
-    // }
-    // )
-    // .catch((error) => {
-    //     console.error("Error:", error);
-    // });
+	// const url = 'https://spotify-message-ranking.vercel.app/api/signup';
+	const url = "http://127.0.0.1:8888/api/signup";
 
+	fetch(url, {
+		method: "POST",
+		// redirect: 'follow',
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ phone_number: phoneNumber }),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.redirect) {
+				window.location.replace(data.redirect);
+			} else {
+				document.getElementById("error-message").innerHTML = "something went wrong";
+			}
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+	// .then(response => {
+	//     console.log(`response: ${response}`)
+	//     if (response.status === 200) {
+	//         document.getElementById('error-message').innerHTML = "You have been signed up for this messaging service!";
+	//         console.log(response.text());
+	//         // console.log("Success! You have been signed up for this service.");
+	//     }
+	//     else if (response.status === 400) {
+	//         document.getElementById('error-message').innerHTML = "This phone number has already been signed up.";
+	//         console.log(`error response - 400: ${response}`);
+	//         // window.location.href = "http://127.0.0.1:5500/retry.html"
+	//     }
+	//     else {
+	//         document.getElementById('error-message').innerHTML = "An error occurred. Please try again later.";
+	//         console.log(`error response - other: ${response}`);
+	//     }
+	// }
+	// )
+	// .catch((error) => {
+	//     console.error("Error:", error);
+	// });
 }
